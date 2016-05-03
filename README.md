bea926a20af9481705a3c374a95f1db55ccb383f
Ben Keith <ben@benlk.com>
2016-05-03 10:55:17 -0400

    Initial commit: copy files from theme-insideenergy, adjust relative paths for plugin use

---
+++ README.md
@@ -0,0 +1,30 @@
+# Pym shortcode
+
+## Sample usage:
+
+```
+[pym src="http://blog.apps.npr.org/pym.js/examples/table/child.html"]
+
+```
+
+## All options:
+
+```
+[pym src="" pymsrc="" pymoptions=""]
+```
+
+`src` is the URL of the page that is to be embedded.
+
+`pymsrc` is optional; only set this if you need to specify a different source than the default. The default pym source is `js/pym.src` in this theme.
+
+`pymoptions` is optional; this should be a javascript object without the surrounding `{}`, and is given in the event that options need to be passed to the `pymParent`. NPR gives [this example](http://blog.apps.npr.org/pym.js/#examples) javascript:
+
+```js
+pym.Parent('example', 'child.html', { xdomain: '*\.npr\.org' });
+```
+
+To do the same thing with this Pym shortcode, you would write:
+
+```
+[pym src="child.html" pymoptions=" xdomain: '*\.npr\.org' "]
+```
---
+++ pym-shortcode.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Pym Shortcode
+Plugin URI: https://github.com/INN/pym-shortcode
+Description: Adds a [pym src=""] shortcode to simplify use of NPR's Pym.js
+Version: 0.1
+Author: The INN Nerds
+Author URI: http://nerds.inn.org/
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * A shortcode to simplify the process of embedding articles using pym.js
+ */
+function pym_shortcode( $atts, $context, $tag ) {
+
+	// generate an ID for this embed, necessary to prevent conflicts
+	global $pym_id;
+	if ( ! isset($pym_id) ) {
+		$pym_id = 0;
+	} else {
+		++$pym_id;
+	}
+
+	$pymsrc = empty($atts['pymsrc']) ? plugins_url( '/js/pym.js', __FILE__ ) : $atts['pymsrc'];
+	$pymoptions = empty($atts['pymoptions']) ? '' : $atts['pymoptions'];
+
+	$src = $atts['src'];
+
+	ob_start();
+
+	echo '<div id="pym_' . $pym_id . '"></div>';
+
+	// If this is the first one on the page, output the pym src
+	// or if the pymsrc is set, output that.
+	if ( $pym_id == 0 || $atts['pymscr'] ) {
+		echo sprintf(
+			'<script src="%s"></script>',
+			$pymsrc
+		);
+	}
+
+	// Output the parent's scripts
+	echo '<script>';
+	echo sprintf(
+		'var pym_%1$s = new pym.Parent(\'%2$s\', \'%3$s\', {%4$s})',
+		$pym_id,
+		"pym_$pym_id",
+		$src,
+		$pymoptions
+	);
+	echo '</script>';
+
+	// What is output to the page
+	$ret = ob_get_clean();
+	return $ret;
+}
+add_shortcode('pym', 'pym_shortcode');
