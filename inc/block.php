cb42d69060c7df200d7a9d2462c72a083085ccdb
Ben Keith <ben@benlk.com>
2018-08-22 17:19:54 -0400

    scaffold zee blocks

--- inc/block.php
+++ inc/block.php
@@ -1,5 +1,55 @@
 <?php
 /**
  * The Gutenberg block for Pym embeds
+ *
+ * @package pym-shortcode
  */
 
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
+ */
+function pym_block_init() {
+	$dir = dirname( dirname( __FILE__ ) );
+
+	$block_js = 'js/block.js';
+	wp_register_script(
+		'pym-block-editor',
+		plugins_url( $block_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		),
+		filemtime( "$dir/$block_js" )
+	);
+
+	$editor_css = 'css/editor.css';
+	wp_register_style(
+		'pym-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'css/style.css';
+	wp_register_style(
+		'pym-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'pym-shortcode/pym', array(
+		'editor_script' => 'pym-block-editor',
+		'editor_style'  => 'pym-block-editor',
+		'style'         => 'pym-block',
+	) );
+}
+add_action( 'init', 'pym_block_init' );
